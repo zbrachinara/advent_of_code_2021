78cf6d5ea35ebbe766021f4717ebcf98f436e875
zbrachinara <zbrachinara@gmail.com>
2021-12-09 11:56:49 -0500

    solve day 1

--- src/days/day_9.rs
+++ src/days/day_9.rs
@@ -1,6 +1,8 @@
 use array2d::Array2D;
+use itertools::{chain, Itertools};
 use std::fs::File;
 use std::io::Read;
+use std::iter::once;
 
 fn format(data: &mut impl Read) -> Array2D<u8> {
     let data = {
@@ -20,9 +22,34 @@ fn format(data: &mut impl Read) -> Array2D<u8> {
     Array2D::from_rows(data.as_slice())
 }
 
-pub fn solution_part1(mut data: File) {
+fn get_near(arr: &Array2D<u8>, (x, y): (usize, usize)) -> Box<[u8]> {
+    let mut it: Box<dyn Iterator<Item = _>> = {
+        let it = [(x + 1, y), (x, y + 1)].into_iter();
+        if x > 0 && y > 0 {
+            Box::new(chain(it, [(x - 1, y), (x, y - 1)]))
+        } else if x > 0 {
+            Box::new(chain(it, once((x - 1, y))))
+        } else if y > 0 {
+            Box::new(chain(it, once((x, y - 1))))
+        } else {
+            Box::new(it)
+        }
+    };
+
+    it.map(|(x, y)| arr.get(x, y))
+        .filter_map(|x| x)
+        .cloned()
+        .collect::<Vec<_>>()
+        .into_boxed_slice()
+}
+
+pub fn solution_part1(mut data: File) -> u32 {
     let data = format(&mut data);
 
-
-    println!("{:?}", data);
+    (0..100)
+        .cartesian_product(0..100)
+        .map(|pos| (data[pos], get_near(&data, pos)))
+        .filter(|(height, near)| near.iter().all(|x| height < x))
+        .map(|(x, _)| (x+1) as u32)
+        .sum()
 }
